64d3aed999e17bfdccc48a6140a32d2fa0a5dd7f
CANFIELD.BRANDIN.W.1078680373.E <bcanfield@miletwo.us>
2024-09-13 16:29:21 -0400

    Initial cleanup and conversion from shadcn docs to simple registry

---
+++ app/(app)/page.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link"
+
+import { Announcement } from "@/components/announcement"
+import {
+  PageActions,
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "@/components/page-header"
+import { siteConfig } from "@/config/site"
+import { Button } from "@/registry/ui/button"
+
+export default function IndexPage() {
+  return (
+    <div className="container relative">
+      <PageHeader>
+        <Announcement />
+        <PageHeaderHeading>Build your component library</PageHeaderHeading>
+        <PageHeaderDescription>
+          Beautifully designed components that you can copy and paste into your
+          apps.
+        </PageHeaderDescription>
+        <PageActions>
+          <Button asChild size="sm">
+            <Link href="/docs">Get Started</Link>
+          </Button>
+          <Button asChild size="sm" variant="ghost">
+            <Link
+              target="_blank"
+              rel="noreferrer"
+              href={siteConfig.links.github}
+            >
+              GitHub
+            </Link>
+          </Button>
+        </PageActions>
+      </PageHeader>
+   
+    </div>
+  )
+}
