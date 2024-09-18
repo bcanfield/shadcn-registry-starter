ce4ba2c8e700b3f0840be0941d2eb7d156fbe60d
CANFIELD.BRANDIN.W.1078680373.E <bcanfield@miletwo.us>
2024-09-18 09:33:15 -0400

    Readme

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,37 +1,6 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
-import {
-  PageActions,
-  PageHeader,
-  PageHeaderDescription,
-  PageHeaderHeading,
-} from "@/components/page-header";
-import { siteConfig } from "@/config/site";
-import { Button } from "@/components/ui/button";
-
-export default function IndexPage() {
-  return (
-    <div className="container relative">
-      <PageHeader>
-        <PageHeaderHeading>Shadcn Registry Starter</PageHeaderHeading>
-        <PageHeaderDescription>
-          Template for creating a custom shadcn component registry
-        </PageHeaderDescription>
-        <PageActions>
-          <Button asChild size="sm">
-            <Link href="/docs">Get Started</Link>
-          </Button>
-          <Button asChild size="sm" variant="ghost">
-            <Link
-              target="_blank"
-              rel="noreferrer"
-              href={siteConfig.links.github}
-            >
-              GitHub
-            </Link>
-          </Button>
-        </PageActions>
-      </PageHeader>
-    </div>
-  );
+export default async function IndexPage() {
+  // Replace this main page with your home page
+  redirect("/docs/");
 }
